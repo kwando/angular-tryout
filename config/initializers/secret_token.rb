e1d3af7d4b934a0c69a51104df7312d728ab48a1
kwando <kwando@merciless.me>
2013-02-07 19:15:38 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularTryout::Application.config.secret_token = '86bb330522e51342b2a448f6a0090d112dafbfb54ce3f763c16bc4d1134b4e5305f3d046b88d9480e7e87c996caaf58a3604bcbb606ceb2f36ea197a41239a49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AngularTryout::Application.config.session_store :cookie_store, key: '_angular-tryout_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AngularTryout::Application.config.session_store :active_record_store
